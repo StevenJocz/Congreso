19c14fff74090276fb51fd96d914fb0d0a995100
Steven Jocz <56320070+StevenJocz@users.noreply.github.com>
2024-12-17 14:19:07 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import * as path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base : 'https://congreso.unac.edu.co',
+  base : 'https://stevenjocz.github.io/Congreso',
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
   },
